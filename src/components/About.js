81dc52760bfeb4d8a511983b5bc6eec34e89ebf2
boluwatifeajayi <bolu4good@gmail.com>
2020-12-24 21:33:06 +0100

    alt problem fixed

--- src/components/About.js
+++ src/components/About.js
@@ -24,7 +24,7 @@ export default function About(){
 
     return (
         <main className="relative">
-            <img src={dark} alt="dark image" className="absolute w-full"/>
+            <img src={dark} className="absolute w-full"/>
             <div className="p-10 lg:pt-48 container mx-auto relative">
                 <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
                     <img src={urlFor(author.authorImage).url()} className="rounded w-32 lg:w-64 lg:h-64 mr-8" alt={author.name}/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import image from "../bat.jpg"
 export default function Home(){
     return (
         <main>
-            <img src={image} alt="background image" className="absolute object-cover w-full h-full"/>
+            <img src={image} className="absolute object-cover w-full h-full"/>
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
                 <h1 className="text-4xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Aloha, I'm Bolu.</h1>
             </section>
